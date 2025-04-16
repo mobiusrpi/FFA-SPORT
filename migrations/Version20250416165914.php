af663f6b2ef3d726a6e85fa716a0a211c968779b
unknown <jtremblet@gmail.com>
2025-04-16 20:07:17 +0200

    add users login

---
+++ migrations/Version20250416165914.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250416165914 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE users ADD last_name VARCHAR(180) NOT NULL, ADD firstname VARCHAR(180) DEFAULT NULL, DROP username, CHANGE is_verified is_verfied TINYINT(1) NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE users ADD username VARCHAR(30) NOT NULL, DROP last_name, DROP firstname, CHANGE is_verfied is_verified TINYINT(1) NOT NULL
+        SQL);
+    }
+}
